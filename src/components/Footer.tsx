d09cd8374f75295f6f91e1ef5aefaa0e69864d5f
Joel Aduma <caesaragen@gmail.com>
2023-04-07 19:04:19 +0300

    format footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,26 +3,29 @@ import Layout from './Layout'
 import Link from 'next/link'
 
 const Footer: React.FC = () => {
-  return (
-    <footer className='w-full border-t-2 border-solid border-black font-medium
+    return (
+        <footer className='w-full border-t-2 border-solid border-black font-medium
     text-lg
     '>
-        <Layout className='py-8 flex items-center justify-between'>
-            <span>
-                &copy; 2021 KaziQuest
-            </span>
-            <span>
-                Rights
-            </span>
-            <Link href={'/'}>
-            KaziQuest
-            </Link>
-            <Link href={'/contact'}>
-            Contact
-            </Link>
-        </Layout>
-    </footer>
-  )
+            <Layout className='py-8 flex items-center justify-between'>
+                <span>
+                    &copy; 2021 KaziQuest
+                </span>
+                <span>
+                    {new Date().getFullYear()}
+                    &copy; All Rights Reserved
+                </span>
+                <div className='flex items-center'>
+                    Built with <span className='text-[red] text-2xl px-1'>&#9825; </span> by  
+                    <Link href={'https://joeladuma.vercel.app/'}className='ml-1 underline underline-offset-2'> Caesar
+                    </Link>
+                </div>
+                <Link href={'/contact'}>
+                    Contact
+                </Link>
+            </Layout>
+        </footer>
+    )
 }
 
 export default Footer
